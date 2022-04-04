4c4abb745bb6065eaa7fc60851cb8cb2ab8363d1
Peril <tanbir15-10420@diu.edu.bd>
2022-04-04 04:40:33 -0400

    User Profile

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('create-Room/', views.createRoom, name="create-room"),
     path('update-Room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-Room/<str:pk>/', views.deleteRoom, name="delete-room"),
